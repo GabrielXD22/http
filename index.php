89e892149db778725a119362d1415b27d9a570e5
PokemonVictorious <45677576+PokemonVictorious@users.noreply.github.com>
2018-12-07 01:18:26 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$config['database']['host'] = "192.168.0.105";
+$config['database']['login'] = "root";
+$config['database']['password'] = "";
+$config['database']['database'] = "poketibia";
+
+ ?>
+<html lang="pt-br">
+  <head>
+    <title>Pokemon Victorious Sua jornada começa aqui!</title>
+    <meta charset="utf-8">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link href="style.css" rel="stylesheet"/>
+
+  </head>
+
+  <body>
+    <img src="\img\painel.png" id="painel" />
+    <img src="\img\menu\menu.png" id="menu_painel"/>
+    <a href="https://www.facebook.com/">
+      <img src="\img\menu\inicio.png" style="opacity:50%" onmouseover="style.opacity=1" onmouseout="" id="inicio"/>
+    </a>
+
+
+      <div class="container">
+        <body background="\img\background.png" bgproperties="fixed">
+
+
+          <body menu>
+
+          </body>
+        </div>
+
+
+  </body>
+</html>
